77476fbb0f38718138f1cff48f3dfa92545c386a
MRCELEBI51 <225542009@firat.edu.tr>
2026-02-24 22:41:41 +0300

    ilgi alanları ve bölüm kısmı eklendi

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,11 @@ function App(){
       <h2>LAB-1</h2>
       <p> Ad Soyad: Muhammet Ali ÇELEBİ</p>
       <p> Ogrenci No: 225542009</p>
+      <p> Bölümüm: Yazılım Mühendisliği</p>
+      <h2>Ilgi Alanlarim</h2>
+      <p>Arabalar</p>
+      <p>Parfümler</p>
+      
     </div>
   )
 }
