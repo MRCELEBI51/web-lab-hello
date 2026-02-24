aa6e0a09505350a486e5a4ac8bbcf77295f6540a
MRCELEBI51 <225542009@firat.edu.tr>
2026-02-24 22:34:23 +0300

    guncelleme: ad soyad ve numara başlıkları eklendi

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App(){
+  return(
+    <div>
+      <h1>Web Tasarimi ve Programlama</h1>
+      <h2>LAB-1</h2>
+      <p> Ad Soyad: Muhammet Ali ÇELEBİ</p>
+      <p> Ogrenci No: 225542009</p>
+    </div>
+  )
+}
+export default App
